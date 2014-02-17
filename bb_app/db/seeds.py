03fab01ae026dd21624a9d4eda838f7af91b1257
jcoupey <julien.coupey@gmail.com>
2014-02-17 14:59:00 +0100

    Adding the list of comments for each post

--- bb_app/db/seeds.py
+++ bb_app/db/seeds.py
@@ -5,4 +5,9 @@ Populate the database with the artificial data below :
 
 """
 
-user1 = User.create(name='admin', password='$admin&123456$')
+user1 = User.create(name='admin', password='$admin&123456$', email='admin@home.com')
+user2 = User.create(name='toto', password='totototo', email='toto@toto.com')
+post1 = Post.create(title = 'Super post de l\'admin' , content ='blablablabla', author = user1)
+post2 = Post.create(title = 'Autre message' , content ='blabla', author = user2)
+comment11 = Comment.create(title = 'Vraiment super' , content ='l\'admin fais vraiment des supers posts', author = user2, post = post1)
+comment12 = Comment.create(title = 'Merci' , content ='merci toto', author = user1, post = post1)
